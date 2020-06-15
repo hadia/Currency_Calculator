7080aa713b8b45de97ec3bc3712276c6de0a5d0e
hadia <hadiamohamed.iti@gmail.com>
2020-06-15 12:37:17 +0200

    Add calculator view

---
+++ app/src/main/java/com/hadia/task/CurrencyCalculator/ui/details/CurrencyCalculatorViewModel.kt
@@ -0,0 +1,9 @@
+package com.hadia.task.CurrencyCalculator.ui.details
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import com.hadia.task.CurrencyCalculator.ui.currencyrates.RateData
+
+class CurrencyCalculatorViewModel() : ViewModel() {
+
+}
